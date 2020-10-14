6a4e4349993f731656112528aae4729601346b15
Edmond Wu <edmond.wu@velocitylight.net>
2020-10-14 11:02:58 +0800

    Update

--- .storybook/preview.js
+++
@@ -1,8 +0,0 @@
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  layout: 'centered',
-  options: {
-    storySort: { order: ['Introduction', 'Layout', 'Components', 'Form', 'Feedback' ]}
-  }
-}
--- src/lib/ThemeProvider/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ThemeProvider as BasicThemeProvider } from 'styled-jss';
-
-const ThemeProvider = ({ theme, children }) => {
-return <BasicThemeProvider theme={theme}>{children}</BasicThemeProvider>
-} 
-
-export default ThemeProvider;
